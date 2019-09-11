854ce79721ab411e71d7690985a44d30f66978ce
Dejan Stojanovic <d.stojanovic@hotmail.com>
2019-09-11 20:42:13 +0400

    Adds comments for the dbcontext mocking

--- src/WebShop.Products.Tests/Data/ProductRepositoryTests.cs
+++ src/WebShop.Products.Tests/Data/ProductRepositoryTests.cs
@@ -18,18 +18,19 @@ namespace WebShop.Products.Tests.Data
         [Fact]
         public void GetByIdAsync_Returns_Product()
         {
+            //Setup DbContext and DbSet mock
             var dbContextMock = new Mock<ProductsDbContext>();
-
             var dbSetMock = new Mock<DbSet<Product>>();
             dbSetMock.Setup(s => s.FindAsync(It.IsAny<Guid>())).Returns(Task.FromResult(new Product()));
-
             dbContextMock.Setup(s => s.Set<Product>()).Returns(dbSetMock.Object);
-            var productRepository = new ProductsRepository(dbContextMock.Object);
 
+            //Execute method of SUT (ProductsRepository)
+            var productRepository = new ProductsRepository(dbContextMock.Object);
             var product = productRepository.GetByIdAsync(Guid.NewGuid()).Result;
+
+            //Assert
             Assert.NotNull(product);
             Assert.IsAssignableFrom<Product>(product);
-
         }
 
         [Fact]
